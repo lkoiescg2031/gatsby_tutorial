2fccb8006ab96df9e2e871a496dd6686cdef3c0e
taehong <lkoiescg2031@naver.com>
2019-10-21 16:04:24 +0900

    tutorial-part7

---
+++ tutorial-part7/src/pages/my-files.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default ({ data }) => (
+  <Layout>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>relativePath</th>
+            <th>prettyeSize</th>
+            <th>extension</th>
+            <th>birthTime</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allFile.edges.map(({ node }, index ) => (
+            <tr key={index}>
+              <td>{node.relativePath}</td>
+              <td>{node.prettySize}</td>
+              <td>{node.extension}</td>
+              <td>{node.birthTime}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
+      }
+    }
+  }
+`
